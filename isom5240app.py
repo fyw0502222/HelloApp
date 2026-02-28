e45d9dde431e9aeef6e5a41a9da1541695591b6a
fyw0502222 <fyw0502@gmail.com>
2026-02-28 17:43:32 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -4,24 +4,24 @@ import streamlit as st
 
 # Function part
 def age_classifier(image_name):
-    age_classifier = pipeline(""image-classification"",
-                              model=""akashmaggon/vit-base-age-classification"")
+    age_classifier = pipeline("image-classification",
+                              model="akashmaggon/vit-base-age-classification")
 
     age_predictions = age_classifier(image_name)
     age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
     return age_predictions
 
 def main():
-    st.write(""Title: Age Classification using ViT"")
+    st.write("Title: Age Classification using ViT")
 
-    image_name = ""middleagedMan.jpg""
+    image_name = "middleagedMan.jpg"
     image_name = Image.open(image_name).convert(""RGB"")
 
     # Display results
-    st.write(""Predicted Age Range:"")
-    st.write(f""Age range: {age_classifier(image_name)[0]['label']}"")
+    st.write("Predicted Age Range:")
+    st.write(f"Age range: {age_classifier(image_name)[0]['label']}")
 
-if __name__ == ""__main__"":
+if __name__ == "__main__":
     main()
 
      
