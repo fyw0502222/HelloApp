d98abd817691bb66284e602077781400ec3dc796
fyw0502222 <fyw0502@gmail.com>
2026-03-14 15:37:21 +0800

    Update isom5240app.py
    Replaced age classification logic with text classification using a pre-trained model. Updated Streamlit integration for displaying predictions.

--- isom5240app.py
+++ isom5240app.py
@@ -1,27 +1,28 @@
-from transformers import pipeline
-from PIL import Image
-import streamlit as st
+from transformers import AutoModelForSequenceClassification
+from transformers import AutoTokenizer
+import torch
+import numpy as np
+import streamlist as st
+# Testing with the saved model
+model2 = AutoModelForSequenceClassification.from_pretrained("CustomModel_yelp",
+                                                            num_labels=5)
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
-# Function part
-def age_classifier(image_name):
-    age_classifier = pipeline("image-classification",
-                              model="prithivMLmods/Age-Classification-SigLIP2")
+# Tokenized testing data
+label = 4 # label = 4
+text = "dr. goldberg offers everything i look for in a general practitioner. he's nice and easy to talk to without being patronizing; he's always on time in seeing his patients; he's affiliated with a top-notch hospital (nyu) which my parents have explained to me is very important in case something happens and you need surgery; and you can get referrals to see specialists without having to see him first. really, what more do you need? i'm sitting here trying to think of any complaints i have about him, but i'm really drawing a blank."
+inputs = tokenizer(text,
+                   padding=True,
+                   truncation=True,
+                   return_tensors='pt')
 
-    age_predictions = age_classifier(image_name)
-    age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
-    return age_predictions
+outputs = model2(**inputs)
+predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
+predictions = predictions.cpu().detach().numpy()
 
-def main():
-    st.write("Title: Age Classification using ViT")
+# Get the index of the largest output value
+max_index = np.argmax(predictions)
 
-    image_name = "middleagedMan.jpg"
-    image_name = Image.open(image_name).convert("RGB")
-
-    # Display results
-    st.write("Predicted Age Range:")
-    st.write(f"Age range: {age_classifier(image_name)[0]['label']}")
-
-if __name__ == "__main__":
-    main()
+st.write(f"The label is {label} and the predicted label is {max_index}")
 
      
